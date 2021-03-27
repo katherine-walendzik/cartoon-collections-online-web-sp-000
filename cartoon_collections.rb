3f7511f004920207b17669b29ec7a54f332c6d30
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-27 16:13:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  if calls.map{|call| call.length > 4}
+  if calls.map{|call| call.length >= 4}
     return true 
   else
     return false
