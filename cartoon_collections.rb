36d7e5399b365a786d516cd8d6aebf4a29f91577
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-27 16:02:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map{|call| call.
 end
 
 def long_planeteer_calls# code an argument here
